7612db5f9a807aa30c5fe57508d0ff01cc1f80eb
Tim Shelburne <shelburt02@gmail.com>
2019-02-07 11:31:12 -0500

    initial commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,22 @@
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import CleanWebpackPlugin from 'clean-webpack-plugin'
+
+export default {
+	module: {
+		rules: [
+			{ test: /\.js$/, exclude: /node_modules/, loader: `babel-loader` },
+			{
+				test: /\.css$/,
+				use: [
+					`style-loader`,
+					`css-loader`
+				]
+			},
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({template: `./src/index.html`}),
+		new CleanWebpackPlugin([`public`]),
+	]
+}
